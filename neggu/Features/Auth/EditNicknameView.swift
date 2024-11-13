dc4f9160a63ee95f4bda1bfc4908a327db8e2983
Jiho <yjh7827@naver.com>
2024-11-13 22:50:51 +0900

    feat: 회원가입 유저 정보 입력뷰 임시 작업

--- neggu/Features/Auth/EditNicknameView.swift
+++ neggu/Features/Auth/EditNicknameView.swift
@@ -10,25 +10,137 @@ import SwiftUI
 struct EditNicknameView: View {
     @StateObject private var viewModel: NicknameEditViewModel
     
+    @State private var step: Int? = 1
+    
     init(viewModel: NicknameEditViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
     }
     
     var body: some View {
         VStack {
-            TextField("닉네임을 입력해주세요.", text: $viewModel.nickname)
+            Capsule()
+                .fill(.black.opacity(0.1))
+                .frame(width: 204, height: 8)
+                .padding(.bottom, 21)
+                .overlay(alignment: .topLeading) {
+                    Capsule()
+                        .fill(.orange40)
+                        .frame(width: CGFloat(step ?? 1) * 68, height: 8)
+                }
             
-            Button("중복확인") {
-                viewModel.requestCheckNickname()
+            Text("STEP 1/3")
+                .negguFont(.caption)
+                .foregroundStyle(.labelAssistive.opacity(0.4))
+                .padding(.bottom, 84)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 0) {
+                    VStack {
+                        Text("닉네임을 입력해주세요!")
+                            .negguFont(.title4)
+                            .foregroundStyle(.labelNormal)
+                        
+                        TextField("닉네임을 입력해주세요.", text: $viewModel.nickname)
+                            .negguFont(.body1b)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .background {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .strokeBorder(.lineNormal, lineWidth: 1)
+                            }
+                            .padding(.horizontal, 44)
+                            .padding(.bottom)
+                        
+                        Text("한글 최대 7자, 영문 14자까지 가능해요")
+                            .negguFont(.caption)
+                            .foregroundStyle(.labelAssistive.opacity(0.4))
+                            .padding(.bottom, 112)
+                        
+            //            Button("중복확인") {
+            //                viewModel.requestCheckNickname()
+            //            }
+            //
+            //            if !viewModel.isDuplicatedNickname {
+            //                Button("닉네임 변경") {
+            //                    viewModel.requestUpdateInfo()
+            //                }
+            //            }
+                        
+                        Text("나이를 알려주세요!")
+                            .negguFont(.title4)
+                            .foregroundStyle(.labelNormal)
+                        
+                        HStack(spacing: 12) {
+                            RoundedRectangle(cornerRadius: 16)
+                                .strokeBorder(.lineNormal)
+                                .frame(width: 100, height: 63)
+                            
+                            Text("살")
+                                .negguFont(.title4)
+                                .foregroundStyle(.labelNormal)
+                        }
+                    }
+                    .containerRelativeFrame(.horizontal)
+                    .id(1)
+                    
+                    VStack {
+                        Text("qwer")
+                            .foregroundStyle(.black)
+                    }
+                    .containerRelativeFrame(.horizontal)
+                    .id(2)
+                    
+                    VStack {
+                        Text("xyz")
+                            .foregroundStyle(.black)
+                    }
+                    .containerRelativeFrame(.horizontal)
+                    .id(3)
+                }
+                .scrollTargetLayout()
+            }
+            .scrollDisabled(true)
+            .scrollPosition(id: $step)
+            
+            Spacer()
+            
+            Button {
+                guard let step, step > 0 && step < 3 else { return }
+                self.step = step + 1
+            } label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.labelAssistive)
+                    .frame(height: 48)
+                    .overlay {
+                        Text("다음으로")
+                            .negguFont(.body1b)
+                            .foregroundStyle(.white)
+                    }
             }
             
-            if !viewModel.isDuplicatedNickname {
-                Button("닉네임 변경") {
-                    viewModel.requestUpdateInfo()
+            HStack {
+                Button {
+                    guard let step, step > 1 else { return }
+                    self.step = step - 1
+                } label: {
+                    HStack(spacing: 0) {
+                        Image(systemName: "chevron.left")
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        
+                        Text("이전으로")
+                            .negguFont(.body2)
+                    }
+                    .foregroundStyle(.labelAlt)
                 }
+                .buttonStyle(PlainButtonStyle())
+                
+                Spacer()
             }
         }
         .padding()
+        .background(.white)
+        .animation(.smooth, value: step)
     }
 }
 
