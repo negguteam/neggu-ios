da76e3572808387b83bc9d42f72c9f36d6478f07
Jiho <yjh7827@naver.com>
2024-11-14 17:48:38 +0900

    feat: LoginView GUI 적용

--- neggu/Features/Auth/LoginView.swift
+++ neggu/Features/Auth/LoginView.swift
@@ -16,20 +16,91 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack {            
-            Button("구글 로그인") {
-                authViewModel.requestGoogleLogin()
-            }
+        VStack {
+            Image(.negguLogo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 180, height: 180)
+                .padding(.top, 240)
+                .padding(.bottom, 150)
             
-            Button("애플 로그인") {
-                authViewModel.requestAppleLogin()
-            }
-            
-            Button("카카오 로그인") {
-                authViewModel.requestKakaoLogin()
+            VStack(spacing: 12) {
+                Button {
+                    authViewModel.requestKakaoLogin()
+                } label: {
+                    HStack(spacing: 24) {
+                        Rectangle()
+                            .fill(.gray10)
+                            .frame(width: 44, height: 44)
+                        
+                        Text("카카오 로그인")
+                            .negguFont(.body2b)
+                            .foregroundStyle(.black)
+                            .frame(maxWidth: .infinity)
+                        
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 56)
+                    .padding(.horizontal, 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.yellow)
+                    }
+                }
+                
+                Button {
+                    authViewModel.requestGoogleLogin()
+                } label: {
+                    HStack(spacing: 24) {
+                        Rectangle()
+                            .fill(.gray10)
+                            .frame(width: 44, height: 44)
+                        
+                        Text("Google 계정으로 계속하기")
+                            .negguFont(.body2b)
+                            .foregroundStyle(.black)
+                            .frame(maxWidth: .infinity)
+                        
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 56)
+                    .padding(.horizontal, 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.white)
+                    }
+                }
+                
+                Button {
+                    authViewModel.requestAppleLogin()
+                } label: {
+                    HStack(spacing: 24) {
+                        Rectangle()
+                            .fill(.gray10)
+                            .frame(width: 44, height: 44)
+                        
+                        Text("Apple 계정으로 계속하기")
+                            .negguFont(.body2b)
+                            .foregroundStyle(.white)
+                            .frame(maxWidth: .infinity)
+                        
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 56)
+                    .padding(.horizontal, 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.black)
+                    }
+                }
             }
         }
-        .padding()
+        .padding(.horizontal, 48)
+        .frame(maxHeight: .infinity)
+        .background(.bgNormal)
         .onChange(of: authViewModel.needEditNickname) { oldValue, newValue in
             if newValue {
                 authCoordinator.push(.editNickname)
@@ -38,6 +109,6 @@ struct LoginView: View {
     }
 }
 
-//#Preview {
-//    LoginView()
-//}
+#Preview {
+    LoginView(authViewModel: AuthViewModel())
+}
