d0f6d0049a2621dee72564b4fff6c940b729ebc2
Jiho <yjh7827@naver.com>
2025-01-20 23:00:15 +0900

    feat: 분위기 입력 화면 수정된 GUI 적용

--- neggu/Features/Auth/SignUpMoodView.swift
+++ neggu/Features/Auth/SignUpMoodView.swift
@@ -12,61 +12,75 @@ struct SignUpMoodView: View {
     @EnvironmentObject private var viewModel: AuthViewModel
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("선호하는 분위기를 알려주세요")
-                    .negguFont(.title4)
-                    .foregroundStyle(.labelNormal)
-                    .padding(.bottom)
-                
-                Text("좋아하는 분위기를 3가지 이상 알려주세요")
-                    .negguFont(.body3)
-                    .foregroundStyle(.labelAssistive)
-                    .padding(.bottom, 48)
-                
-                LazyVGrid(
-                    columns: [GridItem](repeating: .init(.flexible(), spacing: 16), count: 2),
-                    spacing: 14
-                ) {
-                    ForEach(Mood.allCasesArray) { mood in
-                        Button {
-                            if viewModel.moodList.contains(mood) {
-                                viewModel.moodList.remove(mood)
-                            } else {
-                                viewModel.moodList.insert(mood)
-                            }
-                        } label: {
-                            RoundedRectangle(cornerRadius: 16)
-                                .fill(setButtonColor(mood))
-                                .strokeBorder(setButtonBorderColor(mood))
-                                .frame(height: 220)
-                                .overlay {
-                                    Text(mood.rawValue)
+        ScrollViewReader { scrollProxy in
+            ScrollView {
+                VStack {
+                    Text("선호하는 분위기를 알려주세요")
+                        .negguFont(.title4)
+                        .foregroundStyle(.labelNormal)
+                        .padding(.top, 40)
+                        .padding(.bottom)
+                        .id(0)
+                    
+                    Text("좋아하는 분위기를 최대 3가지 알려주세요")
+                        .negguFont(.body3)
+                        .foregroundStyle(.labelAssistive)
+                        .padding(.bottom, 48)
+                    
+                    LazyVGrid(
+                        columns: [GridItem](repeating: .init(.flexible(), spacing: 16), count: 2),
+                        spacing: 14
+                    ) {
+                        ForEach(Mood.allCasesArray) { mood in
+                            Button {
+                                
+                                if viewModel.moodList.contains(mood) {
+                                    viewModel.moodList.removeAll(where: { $0 == mood })
+                                } else {
+                                    if viewModel.moodList.count >= 3 {
+                                        return
+                                    } else {
+                                        viewModel.moodList.append(mood)
+                                    }
                                 }
+                            } label: {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(setButtonColor(mood))
+                                    .strokeBorder(setButtonBorderColor(mood))
+                                    .aspectRatio(8/11, contentMode: .fit)
+                                    .overlay {
+                                        Text(mood.rawValue)
+                                    }
+                            }
                         }
                     }
                 }
+                .padding(.bottom, 120)
             }
-            .padding(.top, 40)
-        }
-        .scrollIndicators(.hidden)
-        .onAppear {
-            print("MoodView")
-            viewModel.canNextStep = false
-            
-            viewModel.nextAction = {
-                viewModel.register {
-                    if $0 { authCoordinator.push(.complete) }
+            .scrollIndicators(.hidden)
+            .padding(.horizontal, 28)
+            .overlay(alignment: .bottom) {
+                LinearGradient(
+                    colors: [
+                        .clear,
+                        Color(red: 248, green: 248, blue: 248, opacity: 0.8),
+                        Color(red: 248, green: 248, blue: 248, opacity: 1)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .frame(height: 240)
+                .allowsHitTesting(false)
+            }
+            .onChange(of: viewModel.step) { _, newValue in
+                if newValue == 4 {
+                    scrollProxy.scrollTo(0, anchor: .top)
                 }
             }
-            
-            viewModel.beforeAction = {
-                viewModel.step -= 1
-                viewModel.moodList.removeAll()
+            .onChange(of: viewModel.moodList) { _, newValue in
+                if viewModel.step != 4 { return }
+                viewModel.canNextStep = newValue.count >= 3
             }
-        }
-        .onChange(of: viewModel.moodList) { _, newValue in
-            viewModel.canNextStep = newValue.count >= 3
         }
     }
     
