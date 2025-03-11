c04a05a58d0ed8ce99c636789b298e9354baa243
Jiho <yjh7827@naver.com>
2025-03-11 21:52:51 +0900

    refactor: Gender 열거형 이니셜라이저 사용을 위해 원시값 수정

--- neggu/Core/Enum/Gender.swift
+++ neggu/Core/Enum/Gender.swift
@@ -8,17 +8,25 @@
 import Foundation
 
 enum Gender: String, CaseIterable, Identifiable, Decodable {
-    case MALE = "남성"
-    case FEMALE = "여성"
+    case MALE
+    case FEMALE
     case UNKNOWN
     
     var id: String { "\(self)" }
+    
+    var title: String {
+        switch self {
+        case .MALE: "남성"
+        case .FEMALE: "여성"
+        default: ""
+        }
+    }
     
     static var allCasesArray: [Self] {
         allCases.filter { $0 != .UNKNOWN }
     }
     
     init(from decoder: Decoder) throws {
-        self = try Gender(rawValue: decoder.singleValueContainer().decode(RawValue.self).lowercased()) ?? .UNKNOWN
+        self = try Gender(rawValue: decoder.singleValueContainer().decode(RawValue.self)) ?? .UNKNOWN
     }
 }
