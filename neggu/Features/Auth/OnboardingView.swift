e077230bebfb5c940c2cf3ba51e55230c19349ca
Jiho <yjh7827@naver.com>
2025-03-25 23:03:11 +0900

    feat: 온보딩 화면 수정된 GUI 적용

--- neggu/Features/Auth/OnboardingView.swift
+++ neggu/Features/Auth/OnboardingView.swift
@@ -8,99 +8,67 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    @State private var scrollID: Int? = 0
+    @State private var tabIndex: Int = 0
     
     let onboardingList: [String] = [
-        "네꾸와 함께 디지털 옷장을\n시작해보세요!",
-        "내 옷장의 옷들로\n코디를 저장할 수 있어요",
-        "친구에게 코디를\n부탁할 수 있어요"
+        "네꾸와 함께\n디지털 옷장을\n시작해보세요",
+        "내 옷들을\n한눈에\n모아보세요",
+        "옷을 꺼내\n코디로\n만들 수 있어요",
+        "친구에게\n코디를\n맡겨보세요"
     ]
     
     var body: some View {
         GeometryReader { proxy in
             VStack(spacing: 0) {
-                ScrollView(.horizontal) {
-                    HStack(spacing: 0) {
-                        ForEach(onboardingList.indices, id: \.self) { index in
-                            Rectangle()
-                                .fill(.gray10)
-                                .frame(width: proxy.size.width)
-                                .overlay {
-                                    Text("\(index)")
-                                }
-                                .id(index)
-                        }
-                    }
-                    .scrollTargetLayout()
-                }
-                .frame(height: proxy.size.width)
-                .scrollIndicators(.hidden)
-                .scrollTargetBehavior(.paging)
-                .scrollPosition(id: $scrollID)
-                
                 HStack(spacing: 4) {
                     ForEach(onboardingList.indices, id: \.self) { index in
                         Capsule()
-                            .fill(scrollID == index ? .labelNormal : .labelInactive)
-                            .frame(width: scrollID == index ? 24 : 8, height: 8)
+                            .fill(tabIndex == index ? .labelRAssistive : .white.opacity(0.2))
+                            .frame(width: tabIndex == index ? 24 : 8, height: 8)
                             .onTapGesture {
-                                scrollID = index
+                                tabIndex = index
                             }
                     }
                 }
                 .frame(height: 56)
                 
-                VStack(alignment: .leading, spacing: 20) {
-                    Text(onboardingList[scrollID ?? 0])
-                        .negguFont(.title2)
-                        .foregroundStyle(.labelNormal)
-                        .padding(.horizontal, 20)
-                    
-                    Spacer()
-                    
-                    Button {
-                        guard let scrollID else { return }
-                        
-                        if scrollID == onboardingList.count - 1 {
-                            UserDefaultsKey.Auth.isFirstVisit = false
-                        } else {
-                            self.scrollID = scrollID + 1
-                        }
-                    } label: {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.negguSecondary)
-                            .frame(height: 56)
-                            .overlay {
-                                Text(scrollID == onboardingList.count - 1 ? "시작하기" : "다음으로")
-                                    .negguFont(.body1b)
-                                    .foregroundStyle(.labelRNormal)
-                            }
+                Text(onboardingList[tabIndex])
+                    .negguFont(.title1)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 28)
+                    .toolbar(.hidden, for: .tabBar)
+                
+                Spacer()
+                
+                Button {
+                    if tabIndex == onboardingList.count - 1 {
+                        UserDefaultsKey.Auth.isFirstVisit = false
+                    } else {
+                        tabIndex += 1
                     }
-                    
-                    if scrollID != 0 {
-                        Button {
-                            guard let scrollID else { return }
-                            self.scrollID = scrollID - 1
-                        } label: {
-                            HStack(spacing: 0) {
-                                Image(systemName: "chevron.left")
-                                    .scaledToFit()
-                                    .frame(width: 24, height: 24)
-                                
-                                Text("이전으로")
-                                    .negguFont(.body2)
-                            }
-                            .foregroundStyle(.labelAlt)
-                            .frame(height: 24)
+                } label: {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.black)
+                        .frame(height: 56)
+                        .overlay {
+                            Text("다음으로")
+                                .negguFont(.body1b)
+                                .foregroundStyle(.labelRNormal)
                         }
-                    }
                 }
-                .padding(.horizontal, 20)
-                .padding(.top, 40)
-                .padding(.bottom, 24)
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 24)
+            .background {
+                Capsule()
+                    .fill(.cyan)
+                    .blur(radius: 100)
+                    .offset(y: proxy.size.height - CGFloat(tabIndex) * proxy.size.height * 0.2)
             }
         }
-        .animation(.smooth, value: scrollID)
+        .background(Color.black)
+        .animation(.smooth, value: tabIndex)
     }
 }
 
