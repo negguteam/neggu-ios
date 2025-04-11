809b2e5748a6566925852c39141f1109d7ecdeb7
Jiho <yjh7827@naver.com>
2025-04-11 22:47:48 +0900

    feat: AppsFlyer 기본 설정

--- neggu/App/AppDelegate.swift
+++ neggu/App/AppDelegate.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseMessaging
+import AppsFlyerLib
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
@@ -16,6 +17,12 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
+        AppsFlyerLib.shared().appsFlyerDevKey = NegguEnv.AppsFlyer.devKey
+        AppsFlyerLib.shared().appleAppID = NegguEnv.AppsFlyer.appleId
+        AppsFlyerLib.shared().appInviteOneLinkID = NegguEnv.AppsFlyer.oneLinkId
+        
+        AppsFlyerLib.shared().delegate = self
+        AppsFlyerLib.shared().deepLinkDelegate = self
         
         UNUserNotificationCenter.current().delegate = self
         Messaging.messaging().delegate = self
@@ -36,6 +43,32 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
         Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppsFlyerLib.shared().start()
+    }
+    
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url)
+        return true
     }
     
 }
@@ -86,6 +119,67 @@ extension AppDelegate: MessagingDelegate {
             object: nil,
             userInfo: dataDict
         )
+    }
+    
+}
+
+
+extension AppDelegate: DeepLinkDelegate {
+    
+    func didResolveDeepLink(_ result: DeepLinkResult) {
+        switch result.status {
+        case .found:
+            guard let deepLink = result.deepLink else { return }
+            let deepLinkString = deepLink.toString()
+            
+            print("[AFSDK] Deep link found: \(deepLinkString)")
+            
+            if result.deepLink?.isDeferred == true {
+                print("[AFSDK] This is a deferred deep link")
+            } else {
+                print("[AFSDK] This is a direct deep link")
+            }
+            
+            print(deepLink.deeplinkValue, deepLink.clickEvent["deep_link_sub1"])
+        case .notFound:
+            print("[AFSDK] Deep link not found")
+        case .failure:
+            print("Error %@", result.error!)
+        }
+        
+    }
+    
+}
+
+extension AppDelegate: AppsFlyerLibDelegate {
+    
+    func onConversionDataSuccess(_ data: [AnyHashable: Any]) {
+        print("onConversionDataSuccess data:")
+        
+        for (key, value) in data {
+            print(key, ":", value)
+        }
+        
+        if let status = data["af_status"] as? String {
+            if status == "Non-organic" {
+                if let sourceID = data["media_source"], let campaign = data["campaign"] {
+                    print("This is a Non-Organic install. Media source: \(sourceID)  Campaign: \(campaign)")
+                }
+            } else {
+                print("This is an organic install.")
+                
+            }
+            
+            if let is_first_launch = data["is_first_launch"] as? Bool, is_first_launch {
+                print("First Launch")
+            } else {
+                print("Not First Launch")
+            }
+        }
+    }
+    
+    func onConversionDataFail(_ error: Error) {
+        print("\(error)")
     }
     
 }
--- neggu/NegguDS/NegguDS.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NegguDS.swift
-//  neggu
-//
-//  Created by 유지호 on 8/3/24.
-//
-
-import Foundation
