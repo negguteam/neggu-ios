f734186c75ee270e71aed9c7d92814bdccbf0fce
Jiho <yjh7827@naver.com>
2025-04-15 00:23:44 +0900

    refactor: iOS 17.3에서 기본 바텀 네비게이션 바 안사라지는 이슈 해결

--- neggu/Features/Root/ContentView.swift
+++ neggu/Features/Root/ContentView.swift
@@ -14,26 +14,57 @@ struct ContentView: View {
     @StateObject private var lookBookViewModel = LookBookViewModel()
     @StateObject private var insightViewModel = InsightViewModel()
     
+    @State private var isTabBarHidden: Bool = false
+    
     var body: some View {
         ZStack(alignment: .bottom) {
-            TabView(selection: $coordinator.activeTab) {
-                NavigationStack(path: $coordinator.closetPath) {
-                    coordinator.buildScene(.closet)
-                        .navigationDestination(for: MainCoordinator.Destination.self) { scene in
-                            coordinator.buildScene(scene)
+            if #available(iOS 18.0, *) {
+                TabView(selection: $coordinator.activeTab) {
+                    Tab.init(value: .closet) {
+                        NavigationStack(path: $coordinator.closetPath) {
+                            coordinator.buildScene(.closet)
+                                .navigationDestination(for: MainCoordinator.Destination.self) { scene in
+                                    coordinator.buildScene(scene)
+                                }
                         }
-                }
-                .tag(NegguTab.closet)
-                .toolbar(.hidden, for: .tabBar)
-                
-                NavigationStack(path: $coordinator.lookbookPath) {
-                    coordinator.buildScene(.lookbook)
-                        .navigationDestination(for: MainCoordinator.Destination.self) { scene in
-                            coordinator.buildScene(scene)
+                        .toolbarVisibility(.hidden, for: .tabBar)
+                    }
+                    
+                    Tab.init(value: .lookbook) {
+                        NavigationStack(path: $coordinator.lookbookPath) {
+                            coordinator.buildScene(.lookbook)
+                                .navigationDestination(for: MainCoordinator.Destination.self) { scene in
+                                    coordinator.buildScene(scene)
+                                }
                         }
+                        .toolbarVisibility(.hidden, for: .tabBar)
+                    }
                 }
-                .tag(NegguTab.lookbook)
-                .toolbar(.hidden, for: .tabBar)
+            } else {
+                TabView(selection: $coordinator.activeTab) {
+                    NavigationStack(path: $coordinator.closetPath) {
+                        coordinator.buildScene(.closet)
+                            .navigationDestination(for: MainCoordinator.Destination.self) { scene in
+                                coordinator.buildScene(scene)
+                            }
+                    }
+                    .tag(NegguTab.closet)
+                    .background {
+                        if !isTabBarHidden {
+                            HideTabBar {
+                                isTabBarHidden = true
+                            }
+                        }
+                    }
+                    
+                    NavigationStack(path: $coordinator.lookbookPath) {
+                        coordinator.buildScene(.lookbook)
+                            .navigationDestination(for: MainCoordinator.Destination.self) { scene in
+                                coordinator.buildScene(scene)
+                            }
+                    }
+                    .tag(NegguTab.lookbook)
+                }
             }
             
             if coordinator.isGnbOpened {
@@ -62,8 +93,49 @@ struct ContentView: View {
         .environmentObject(lookBookViewModel)
         .environmentObject(insightViewModel)
         .onOpenURL { url in
-            let urlComponents = URLComponents(string: url.absoluteString)
-            print(urlComponents?.scheme, urlComponents?.host, urlComponents?.path, urlComponents?.queryItems)
+//            let urlComponents = URLComponents(string: url.absoluteString)
+//            print(urlComponents?.scheme, urlComponents?.host, urlComponents?.path, urlComponents?.queryItems)
         }
     }
+}
+
+
+fileprivate struct HideTabBar: UIViewRepresentable {
+    var result: () -> Void
+    
+    init(result: @escaping () -> Void) {
+        UITabBar.appearance().isHidden = true
+        self.result = result
+    }
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        
+        DispatchQueue.main.async {
+            if let tabController = view.tabController {
+                UITabBar.appearance().isHidden = false
+                tabController.tabBar.isHidden = true
+                result()
+            }
+        }
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
+
+fileprivate extension UIView {
+    
+    var tabController: UITabBarController? {
+        if let controller = sequence(
+            first: self,
+            next: { $0.next }).first(where: { $0 is UITabBarController }) as? UITabBarController {
+            return controller
+        }
+        
+        return nil
+    }
+    
 }
