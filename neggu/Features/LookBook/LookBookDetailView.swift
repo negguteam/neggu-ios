c36905f772d862caa97a8e6fb1dde6d163bf0706
Jiho <yjh7827@naver.com>
2025-04-19 03:31:04 +0900

    style: UINavigationController Extension 분리

--- neggu/Features/LookBook/LookBookDetailView.swift
+++ neggu/Features/LookBook/LookBookDetailView.swift
@@ -258,20 +258,6 @@ struct LookBookDetailView: View {
 }
 
 
-extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
-    
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
-    
-    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return viewControllers.count > 1
-    }
-    
-}
-
-
 struct SizeKey: PreferenceKey {
     static var defaultValue: CGFloat = .zero
     
