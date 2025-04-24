e64c4c40bb940418fe0c6c822f042895c13cfbb1
Jiho <yjh7827@naver.com>
2025-04-25 04:12:14 +0900

    chores: 룩북 수정 API 나오기 전까지 편집하기 비활성

--- neggu/Features/LookBook/View/LookBookDetailView.swift
+++ neggu/Features/LookBook/View/LookBookDetailView.swift
@@ -60,10 +60,10 @@ struct LookBookDetailView: View {
                                 }
                             }
                             
-                            Button("편집하기") {
-                                let editingClothes = lookBook.lookBookClothes.map { $0.toLookBookItem() }
-                                coordinator.fullScreenCover = .lookbookRegister(editingClothes: editingClothes)
-                            }
+//                            Button("편집하기") {
+//                                let editingClothes = lookBook.lookBookClothes.map { $0.toLookBookItem() }
+//                                coordinator.fullScreenCover = .lookbookRegister(editingClothes: editingClothes)
+//                            }
                             
                             Button("삭제하기", role: .destructive) {
                                 showDeleteAlert = true
