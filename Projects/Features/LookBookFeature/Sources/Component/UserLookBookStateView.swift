a07038641ffe4926776eaea784c4761d9a094251
Jiho <yjh7827@naver.com>
2025-07-05 17:50:31 +0900

    feat: UserProfileView 디자인 수정

--- Projects/Features/LookBookFeature/Sources/Component/UserLookBookStateView.swift
+++ Projects/Features/LookBookFeature/Sources/Component/UserLookBookStateView.swift
@@ -15,131 +15,70 @@ struct UserLookBookStateView: View {
     
     let profile: UserProfileEntity
     let lookBookState: UserLookBookState
-    var scrollProxy: ScrollViewProxy
     
     var body: some View {
-        HStack(spacing: 12) {
-            switch lookBookState {
-            case .available(let lookBook):
-                RoundedRectangle(cornerRadius: 16)
-                    .fill(.white)
+        switch lookBookState {
+        case .available(let lookBook):
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.white)
+                .aspectRatio(3/4, contentMode: .fit)
+                .overlay {
+                    CachedAsyncImage(lookBook.imageUrl)
+                        .overlay(alignment: .bottom) {
+                            Text(lookBookDateString)
+                                .negguFont(.body3b)
+                                .foregroundStyle(.labelRNormal)
+                                .frame(height: 30)
+                                .padding(.horizontal)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .fill(.black.opacity(0.4))
+                                }
+                                .padding(.bottom, 12)
+                        }
+                }
+                .clipped()
+                .onTapGesture {
+                    coordinator.push(.lookBookDetail(id: lookBook.id))
+                }
+        default:
+            Button {
+                switch lookBookState {
+                case .needClothes:
+                    Task {
+                        coordinator.rootCoordinator?.gnbState = .clothes
+                        coordinator.rootCoordinator?.isGnbOpened = true
+                    }
+                default:
+                    coordinator.presentFullScreen(.lookBookRegister)
+                }
+            } label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.bgInactive)
+                    .strokeBorder(
+                        .lineAlt,
+                        style: StrokeStyle(
+                            lineWidth: 2,
+                            dash: [4, 4]
+                        )
+                    )
                     .aspectRatio(3/4, contentMode: .fit)
                     .overlay {
-                        CachedAsyncImage(lookBook.imageUrl)
-                            .overlay(alignment: .bottom) {
-                                Text(lookBookDateString)
-                                    .negguFont(.body3b)
-                                    .foregroundStyle(.labelRNormal)
-                                    .frame(height: 30)
-                                    .padding(.horizontal)
-                                    .background {
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .fill(.black.opacity(0.4))
-                                    }
-                                    .padding(.bottom, 12)
+                        Text(lookBookStateString)
+                            .negguFont(.title4)
+                            .foregroundStyle(.labelInactive)
+                            .multilineTextAlignment(.center)
+                        
+                        Circle()
+                            .fill(.negguSecondary)
+                            .frame(width: 24, height: 24)
+                            .overlay {
+                                Image(systemName: "plus")
+                                    .bold()
+                                    .foregroundStyle(.white)
                             }
+                            .offset(y: 64)
                     }
-                    .clipped()
-                    .onTapGesture {
-                        coordinator.push(.lookBookDetail(id: lookBook.id))
-                    }
-            default:
-                Button {
-                    switch lookBookState {
-                    case .needClothes:
-                        Task {
-                            coordinator.rootCoordinator?.gnbState = .clothes
-                            coordinator.rootCoordinator?.isGnbOpened = true
-                        }
-                    default:
-                        coordinator.presentFullScreen(.lookBookRegister)
-                    }
-                } label: {
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(.bgInactive)
-                        .strokeBorder(
-                            .lineAlt,
-                            style: StrokeStyle(
-                                lineWidth: 2,
-                                dash: [4, 4]
-                            )
-                        )
-                        .aspectRatio(3/4, contentMode: .fit)
-                        .overlay {
-                            Text(lookBookStateString)
-                                .negguFont(.title4)
-                                .foregroundStyle(.labelInactive)
-                                .multilineTextAlignment(.center)
-                            
-                            Circle()
-                                .fill(.negguSecondary)
-                                .frame(width: 24, height: 24)
-                                .overlay {
-                                    Image(systemName: "plus")
-                                        .bold()
-                                        .foregroundStyle(.white)
-                                }
-                                .offset(y: 64)
-                        }
-                }
-            }
-            
-            VStack {
-                Button {
-                    coordinator.rootCoordinator?.activeTab = .closet
-                } label: {
-                    HStack(spacing: 24) {
-                        VStack {
-                            NegguImage.Icon.shirtFill
-                                .frame(width: 24, height: 24)
-                                .foregroundStyle(.labelNormal)
-                            
-                            Text("의상")
-                                .negguFont(.body2b)
-                                .foregroundStyle(.labelNormal)
-                        }
-                        
-                        Text("\(profile.clothes.count)벌")
-                            .negguFont(.body1b)
-                            .foregroundStyle(.labelAlt)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(.horizontal, 24)
-                    .padding(.vertical, 28)
-                    .background {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.white)
-                    }
-                }
-                
-                Button {
-                    withAnimation(.smooth) {
-                        scrollProxy.scrollTo("LookBook", anchor: .top)
-                    }
-                } label: {
-                    HStack(spacing: 24) {
-                        VStack {
-                            NegguImage.Icon.closetFill
-                                .frame(width: 24, height: 24)
-                                .foregroundStyle(.labelNormal)
-                            
-                            Text("룩북")
-                                .negguFont(.body2b)
-                                .foregroundStyle(.labelNormal)
-                        }
-                        
-                        Text("\(profile.lookBooks.count)개")
-                            .negguFont(.body1b)
-                            .foregroundStyle(.labelAlt)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(.horizontal, 24)
-                    .padding(.vertical, 28)
-                    .background {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.white)
-                    }
-                }
             }
         }
     }
--- Projects/Features/LookBookFeature/Sources/Component/UserProfileHeader.swift
+++ Projects/Features/LookBookFeature/Sources/Component/UserProfileHeader.swift
@@ -14,41 +14,26 @@ struct UserProfileHeader: View {
     let profile: UserProfileEntity
     
     var body: some View {
-//        if let mood = profile.mood.first {
-//            HStack(spacing: 4) {
-//                NegguImage.Icon.titleBadgeIcon
-//                
-//                Text(mood.title + "에 관심이 많은")
-//                    .negguFont(.body2b)
-//            }
-//            .foregroundStyle(.labelNormal)
-//            .frame(height: 28)
-//            .padding(.horizontal, 8)
-//            .background {
-//                NegguImage.Background.gnbBg
-//                    .resizable()
-//                    .clipShape(.rect(cornerRadius: 8))
-//            }
-//        }
-        
-        HStack(spacing: 16) {
-            Text("\(profile.nickname)님\n안녕하세요!")
-                .negguFont(.title2)
-                .foregroundStyle(.labelNormal)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Group {
-                if let profileImage = profile.profileImage {
-                    CachedAsyncImage(profileImage)
-                }
+        VStack(alignment: .leading, spacing: 32) {
+            HStack(spacing: 16) {
+                Text("\(profile.nickname)님\n안녕하세요")
+                    .negguFont(.title2)
+                    .foregroundStyle(.labelRNormal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                NegguImage.Icon.bigNegguStar
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
             }
-            .frame(width: 74, height: 74)
-            .clipShape(.circle)
+            
+            Text("옷장이 잘 채워지고 있어요!")
+                .negguFont(.body2b)
+                .foregroundStyle(.labelInactive)
         }
-        
-        Text("옷장이 잘 채워지고 있어요!")
-            .negguFont(.body2b)
-            .foregroundStyle(.labelAssistive)
-            .padding(.bottom, 20)
+        .padding(.horizontal)
+        .padding(.vertical, 24)
+        .background(.black)
+        .clipShape(.rect(cornerRadius: 16))
     }
 }
